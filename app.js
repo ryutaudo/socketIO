8e89174829b6078d1d098b7b97bac10ccf7652c4
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2017-12-22 10:21:08 +0900

    simple app using socket.io

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');  
+var app = express();  
+var server = require('http').createServer(app);  
+var io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/node_modules'));  
+app.get('/', function(req, res,next) {  
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(4200);
+
+io.on('connection', function(client) {  
+  console.log('Client connected...');
+
+  client.on('join', function(data) {
+      console.log(data);
+      client.emit('messages', 'Hello from server');
+  });
+})
+
+// io.on('connection', function(client) {  
+//   console.log('Client connected...');
+
+//   client.on('join', function(data) {
+//       console.log(data);
+//       client.emit('messages', 'Hello from server');
+//   });
+// })
